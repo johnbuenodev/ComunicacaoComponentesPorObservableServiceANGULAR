0909f41fba9e80949725be05d1fabdea61e5a653
johnbuenodev <johnbuenodev@gmail.com>
2021-12-12 18:15:48 -0300

    Adicionado cancelamento da inscrição do Observable.suscribe vai funcionar somente quando por um componente que desaparece da tela. Exemplo: sair do componente pagina e acessar outro componente

--- src/app/componente2/componente2.component.ts
+++ src/app/componente2/componente2.component.ts
@@ -1,5 +1,5 @@
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Comunicacao1Service } from '../service/comunicacao1.service';
 
 @Component({
@@ -7,20 +7,26 @@ import { Comunicacao1Service } from '../service/comunicacao1.service';
   templateUrl: './componente2.component.html',
   styleUrls: ['./componente2.component.scss']
 })
-export class Componente2Component implements OnInit {
+export class Componente2Component implements OnInit, OnDestroy {
 
   getRegId?: number;
   getRegName?: string;
+  comunicacao1Unsuscription!: Subscription;
 
   constructor(
     private comunicacao1Service : Comunicacao1Service
   ) { }
 
   ngOnInit(): void {
-    this.comunicacao1Service.valorCompoonente1$.subscribe( res => {
+    this.comunicacao1Unsuscription = this.comunicacao1Service.valorCompoonente1$.subscribe( res => {
       this.getRegId = res.id;
       this.getRegName = res.name;   
     });
   }
+
+  ngOnDestroy(): void {
+    console.log("Cancelar a inscrição!");
+    this.comunicacao1Unsuscription.unsubscribe();
+  } 
 
 }
